91face34edb22c3408827e2d754e22775e44957f
Kathiari <kevinkathiari0598@gmail.com>
2022-12-09 18:28:03 -0800

    c language

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/*The program prints alphabet in lowercase*/
+/**
+ * main - The entry point of program
+ *
+ * Description - program prints alphabets in lowercase
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+	return (0);
+}
