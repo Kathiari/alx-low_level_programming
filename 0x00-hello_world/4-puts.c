b229d7bb7ace15ee62a37d65370cdf6035a7b297
Kathiari <kevinkathiari0598@gmail.com>
2022-12-11 22:44:05 -0800

    c language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
-
-int main()
+/*this is a program that displays strings*/
+/**
+ * main -the entry point
+ *
+ * Return: Always 0 succeses
+ */
+int main(void)
 {
-	char program[]="Programming is like building a multilingual puzzle";
-	puts(program);
-	return(0);
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
 }
