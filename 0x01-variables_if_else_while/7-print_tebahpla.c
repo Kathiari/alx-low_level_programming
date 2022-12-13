0af778e70b16b1e579f112168762459404aa41f1
Kathiari <kevinkathiari0598@gmail.com>
2022-12-13 11:53:35 -0800

    c language

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/* This program prints alphabet in reverse order */
+/**
+ * main - entry point of program
+ *
+ * Return: Produces void value
+ */
+int main(void)
+{
+	char z = 'z';
+
+	while (z >= 'a' )
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+	return (0);
+}
