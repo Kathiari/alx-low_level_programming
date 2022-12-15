1bcc5206dbaea1c581a37e277a9ce28ab78d380c
Kathiari <kevinkathiari0598@gmail.com>
2022-12-15 02:46:58 -0800

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,11 @@
-#include<stdio.h>
-#include<unistd.h>
+#include "main.h"
 /* This program prints _putchar */
 /**
  * main - entry point of program
  *
  * Return: returns a null value
  */
-int _putchar(char c);
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
 int main(void)
 {
 	char str[] = "_putchar";
