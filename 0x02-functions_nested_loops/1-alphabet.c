5d5367099afce50e9dc18d56ba513b96ac010034
Kathiari <kevinkathiari0598@gmail.com>
2022-12-15 03:34:28 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ *
+ * Return: returns null value.
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+	
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+
+	_putchar('\n');
+}
