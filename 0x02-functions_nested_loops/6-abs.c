3bc05608aa17f3102e0e86fd1c520c899d6d571a
Kathiari <kevinkathiari0598@gmail.com>
2022-12-29 08:51:22 -0800

    nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,17 +4,15 @@
  * _abs - this func computes absolute value
  *
  * @c: first value
- * @d: decond value
  * Return: Always 0
  */
 
 int _abs(int c)
 {
-	int d = -1;
 
 	if (c < 0)
 	{
-		c = c * d;
+		c = c * -1;
 	}
 	else
 	{
